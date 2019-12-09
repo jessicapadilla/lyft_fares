594ce1ec95fbbfd07c243771cf4e3d0679f3c492
Jessica Padilla <uconnjess@gmail.com>
2019-12-09 14:37:53 -0500

    edited code

--- code.R
+++ code.R
@@ -1,8 +1,8 @@
 ## load libraries
 library(tidyverse)
 
-## assign the link for the lyft data
-lyft_url <- "https://github.com/jessicapadilla/uber_lyft/blob/master/lyft.csv.zip?raw=true"
+## assign the link for the lyft zip folder
+lyft_url <- "https://github.com/jessicapadilla/uber_and_lyft/blob/master/lyft.csv.zip?raw=true"
 
 ## create a temporary file so the data can be downloaded into it
 lyft_temp <- tempfile()
@@ -19,8 +19,8 @@ lyft <- read.csv("lyft.csv", sep = ",", header = TRUE)
 ## check the structure of the data
 str(lyft)
 
-## assign the link for the uber data
-uber_url <- "https://github.com/jessicapadilla/uber_lyft/blob/master/uber.csv.zip?raw=true"
+## assign the link for the uber zip folder
+uber_url <- "https://github.com/jessicapadilla/uber_and_lyft/blob/master/uber.csv.zip?raw=true"
 
 ## create a temporary file so the data can be downloaded into it
 uber_temp <- tempfile()
@@ -52,7 +52,10 @@ identical(rideshare$visibility, rideshare$visibility.1)
 ## remove the visibility.1 and id columns
 ## rename some of the other columns
 rideshare <- rideshare %>% select(-c(id, visibility.1)) %>%
-  rename(apparent_temperature = apparentTemperature,
+  rename(company = cab_type,
+         car_type = name,
+         apparent_temperature = apparentTemperature,
+         weather_summary = short_summary,
          precip_intensity = precipIntensity,
          precip_probability = precipProbability,
          wind_speed = windSpeed,
@@ -67,4 +70,55 @@ rideshare <- rideshare %>% select(-c(id, visibility.1)) %>%
 ## remove rows that do not have prices listed
 rideshare <- na.omit(rideshare, cols = price)
 
-rideshare %>% ggplot(aes(weekday, price)) + geom_jitter()
+names(rideshare)
+
+rideshare %>% 
+  ggplot(aes(weather_summary, price)) +
+  geom_bar(stat = "identity")
+
+Black, Black SUV, UberPool, UberX, UberXL, WAV
+
+unique(rideshare$weather_summary)
+
+## check price ranges between companies and their different services
+## for uber
+unique(rideshare$car_type)
+
+rideshare %>% filter(company == "Uber") %>%
+  ggplot(aes(car_type, price)) + geom_jitter()
+## UberPool is the least and Black SUV is the most
+
+## for lyft
+rideshare %>% filter(company == "Lyft") %>%
+  ggplot(aes(car_type, price)) + geom_jitter()
+## Shared is least and Lux Black XL is most
+
+
+
+## check price for hours, days, and months
+## for uber
+rideshare %>% filter(company == "Uber") %>%
+  ggplot(aes(hour, price, col = car_type)) + geom_jitter()
+
+ggplot(aes(fertility, life_expectancy, col = continent)) + geom_point() + facet_grid(. ~ year)
+
+
+
+
+## create bar plots for destination
+## create bar plots for source
+## create bar plots for weather summary
+## check how distance affects price
+## check how temperature affects surge multiplier, price
+## check how precipitation affects surge multiplier, price
+## check how humidity affects surge multiplier, price
+## check how wind speed affects surge multiplier, price
+## check how visibility affects surge multiplier, price
+## check how wind_bearing affects surge multiplier, price
+## check how cloud_cover affects surge multiplier, price
+## check how weekday affects surge multiplier, price
+## compare prices between companies
+## correlation
+## regression
+## check decision trees
+
